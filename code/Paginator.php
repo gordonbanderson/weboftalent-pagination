aa2a0585c60ae0a14d00aaea0b01d7df7376311b
Gordon Anderson <gordon.b.anderson@gmail.com>
2012-09-26 15:56:26 +0700

    ENHANCEMENT: Extra pagination methods, including search by type

--- code/Paginator.php
+++ code/Paginator.php
@@ -20,25 +20,103 @@ class Paginator_Controller_Extension extends Extension {
     <% end_control %>
   </code>
   */
-	function PagedChildren($klazz, $childrenPerPage = 10) {
- 
+  function PagedChildren( $klazz, $childrenPerPage = 10 ) {
+
     $pageLength = $childrenPerPage;
+
     $parentID = $this->owner->ID;
+    $offset = 0;
+    if ( isset( $this->PagedOffset ) ) {
+      $offset = $this->PagedOffset;
+    }
 
-    $start = isset($_GET['start']) ? (int)$_GET['start'] : 0;
-    $total = DB::query("SELECT COUNT(*) FROM SiteTree where ParentID=".$parentID)->value();
-    $NumberPages = 1 + ($total / $pageLength);
+    $start = isset( $_GET['start'] ) ? (int)( Convert::raw2sql( $_GET['start'] ) ) : 0;
+    $total = DB::query( "SELECT COUNT(*) FROM SiteTree where ParentID=".$parentID )->value();
+    $total = $total - $offset;
+    $NumberPages = 1 + ( $total / $pageLength );
 
-    $results= DataObject::get($klazz, 
-      "ParentID=".$parentID,//filter
-      '',//$sort,//sort
+    $this->PageNumber = 1+$start/$pageLength;
+
+    $results= DataObject::get( $klazz,
+      "ParentID=".$parentID, //filter
+      '', //$sort,//sort
       //'',
       '',
-      $start.','.$pageLength//limit
-      );
+      $start+$offset.','.$pageLength//limit
+    );
 
-      $this->lastPagedResults = $results;
-      return $results;
+    $results->pageStart = $results->pageStart-$offset;
+
+    $ctr = $start+1;
+    foreach ( $results as $result ) {
+      $result->IteratorPosition = $ctr;
+      $ctr = $ctr + 1;
+    }
+
+    $this->lastPagedResults = $results;
+    return $results;
+  }
+
+
+  function PagedDataObjectsByClassName( $klazz, $childrenPerPage = 10 ) {
+
+    $pageLength = $childrenPerPage;
+
+    $parentID = $this->owner->ID;
+    $offset = 0;
+    if ( isset( $this->PagedOffset ) ) {
+      $offset = $this->PagedOffset;
+    }
+
+    $start = isset( $_GET['start'] ) ? (int)( Convert::raw2sql( $_GET['start'] ) ) : 0;
+    $total = DB::query( "SELECT COUNT(*) FROM SiteTree where ClassName='$klazz'" )->value();
+    $total = $total - $offset;
+    $NumberPages = 1 + ( $total / $pageLength );
+
+    $this->PageNumber = 1+$start/$pageLength;
+
+    $results= DataObject::get( $klazz,
+      '', //filter
+      '', //$sort,//sort
+      //'',
+      '',
+      $start+$offset.','.$pageLength//limit
+    );
+
+    if ( $results ) {
+      $results->pageStart = $results->pageStart-$offset;
+
+      $ctr = $start+1;
+      foreach ( $results as $result ) {
+        $result->IteratorPosition = $ctr;
+        $ctr = $ctr + 1;
+      }
+    }
+
+
+
+    $this->lastPagedResults = $results;
+    return $results;
+  }
+
+
+
+
+  function SetPagedOffset( $newoffset ) {
+    $this->PagedOffset = $newoffset;
+  }
+
+
+  function PagedChildrenAllButFirst() {
+
+  }
+
+
+  /*
+  The current page number.  This is only populated after the call to PagedChildren
+  */
+  function PageNumber() {
+    return $this->PageNumber; // variable, not the method, populated when loading the pages items
   }
 
 
